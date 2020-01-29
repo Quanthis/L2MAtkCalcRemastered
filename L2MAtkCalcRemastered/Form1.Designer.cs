077ecc52fd5deb9ccbc4fed011d07ee5eb8d9ce7
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-01-29 16:15:20 +0100

    First steps made

--- L2MAtkCalcRemastered/Form1.Designer.cs
+++ L2MAtkCalcRemastered/Form1.Designer.cs
@@ -28,13 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ApoCaster = new System.Windows.Forms.Button();
+            this.OwnMAttack = new System.Windows.Forms.TextBox();
+            this.ApoCasterResult = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.IsApoCasterBlessed = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // ApoCaster
+            // 
+            this.ApoCaster.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ApoCaster.BackgroundImage = global::L2MAtkCalcRemastered.Properties.Resources.ApoCaster;
+            this.ApoCaster.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ApoCaster.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ApoCaster.Location = new System.Drawing.Point(100, 100);
+            this.ApoCaster.Name = "ApoCaster";
+            this.ApoCaster.Size = new System.Drawing.Size(77, 77);
+            this.ApoCaster.TabIndex = 0;
+            this.ApoCaster.UseVisualStyleBackColor = true;
+            this.ApoCaster.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // OwnMAttack
+            // 
+            this.OwnMAttack.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.OwnMAttack.ForeColor = System.Drawing.Color.Black;
+            this.OwnMAttack.Location = new System.Drawing.Point(910, 800);
+            this.OwnMAttack.Name = "OwnMAttack";
+            this.OwnMAttack.Size = new System.Drawing.Size(100, 20);
+            this.OwnMAttack.TabIndex = 1;
+            this.OwnMAttack.Text = "0";
+            this.OwnMAttack.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ApoCasterResult
+            // 
+            this.ApoCasterResult.Location = new System.Drawing.Point(100, 200);
+            this.ApoCasterResult.Name = "ApoCasterResult";
+            this.ApoCasterResult.Size = new System.Drawing.Size(77, 20);
+            this.ApoCasterResult.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 184);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Result: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(100, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Apocalypse Caster";
+            // 
+            // IsApoCasterBlessed
+            // 
+            this.IsApoCasterBlessed.AutoSize = true;
+            this.IsApoCasterBlessed.Location = new System.Drawing.Point(100, 61);
+            this.IsApoCasterBlessed.Name = "IsApoCasterBlessed";
+            this.IsApoCasterBlessed.Size = new System.Drawing.Size(63, 17);
+            this.IsApoCasterBlessed.TabIndex = 5;
+            this.IsApoCasterBlessed.Text = "Blessed";
+            this.IsApoCasterBlessed.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackgroundImage = global::L2MAtkCalcRemastered.Properties.Resources.Shot00113;
+            this.ClientSize = new System.Drawing.Size(1904, 962);
+            this.Controls.Add(this.IsApoCasterBlessed);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ApoCasterResult);
+            this.Controls.Add(this.OwnMAttack);
+            this.Controls.Add(this.ApoCaster);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(1920, 1080);
+            this.Name = "Form1";
+            this.Text = "L2MAtkCalc 2.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ApoCaster;
+        private System.Windows.Forms.TextBox OwnMAttack;
+        private System.Windows.Forms.Label ApoCasterResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox IsApoCasterBlessed;
     }
 }
 
--- L2MAtkCalcRemastered/Form1.cs
+++ L2MAtkCalcRemastered/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using static System.Convert;
 
 namespace L2MAtkCalcRemastered
 {
@@ -15,6 +17,55 @@ namespace L2MAtkCalcRemastered
         public Form1()
         {
             InitializeComponent();
+        }
+
+        readonly decimal factor = 31.47M;
+
+        private decimal CalculateMAtk(decimal OwnAttack, decimal weaponAttack)
+        {
+            return OwnAttack + weaponAttack * factor;
+        }
+
+        private decimal ConvertOwnAttack()
+        {
+            decimal result;
+
+            try
+            {
+                result = ToDecimal(OwnMAttack.Text);
+                if (result == 0)
+                {
+                    MessageBox.Show("I don't think you attack is so low, try inserting it again :)", "Warning");
+                }
+                return result;
+            }
+            catch(FormatException ex)
+            {
+                MessageBox.Show($"Field {nameof(OwnMAttack)} cannot be empty and can only contain numbers!", "Error!");
+                return 0;
+            }
+            catch(OverflowException)
+            {
+                MessageBox.Show("Have you really become a god among races?", "Error - unexpectedly high value");
+                return 0;
+            }
+            catch(Exception)
+            {
+                MessageBox.Show(@"Unexpected exception! Please report that issue on github: ", "Error!");
+                
+            }
+        }
+
+        private string ConvertToSendableForm(decimal sentValue)
+        {
+            string result = sentValue.ToString();
+            return result;
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            decimal weaponAttack = 293;
+            ApoCasterResult.Text = ConvertToSendableForm(CalculateMAtk(ConvertOwnAttack(), weaponAttack));
         }
     }
 }
